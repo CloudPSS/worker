1a3c7f564677d68f1a40aac8c07eb420baba6c11
OpportunityLiu <opportunity@live.in>
2026-01-09 14:57:36 +0800

    feat: cache factory result

--- src/pool/factory.ts
+++ src/pool/factory.ts
@@ -24,8 +24,11 @@ function isURL(input: unknown): input is URL {
     return typeof input == 'object' && input != null && typeof (input as URL).href == 'string';
 }
 
+/** Worker factory */
+type WorkerFactory = () => MaybeAsync<Worker>;
+
 /** Create a worker from url */
-async function createWorkerFromUrl(url: string | URL, options: WorkerOptions): Promise<Worker> {
+async function createWorkerFromUrl(url: string | URL, options: WorkerOptions): Promise<WorkerFactory> {
     const urlStr = typeof url == 'string' ? url : url.href;
     if (urlStr.startsWith('blob:') && typeof process == 'object' && typeof process?.getBuiltinModule == 'function') {
         // Node.js environment with blob URL
@@ -34,52 +37,52 @@ async function createWorkerFromUrl(url: string | URL, options: WorkerOptions): P
         if (blob == null) {
             throw new Error(`Cannot resolve blob URL: ${urlStr}`);
         }
-        return await createWorkerFromSource(blob, options);
+        return await createWorkerFromSource(await blob.text(), options);
     }
-    return new WorkerPolyfill(urlStr, options) as Worker;
+    return () => new WorkerPolyfill(urlStr, options) as Worker;
 }
 
 /** Create a worker from source code */
-async function createWorkerFromSource(source: string | Blob, options: WorkerOptions): Promise<Worker> {
-    if (typeof Buffer == 'function') {
-        const src = typeof source == 'string' ? source : await source.text();
-        const base64 = Buffer.from(src, 'utf8').toString('base64');
-        const dataUrl = `data:text/javascript;base64,${base64}`;
-        return createWorkerFromUrl(dataUrl, options);
-    } else {
-        const blob = typeof source == 'string' ? new Blob([source], { type: 'application/javascript' }) : source;
-        const url = URL.createObjectURL(blob);
-        try {
-            return createWorkerFromUrl(url, options);
-        } finally {
-            setTimeout(() => URL.revokeObjectURL(url), 1000);
-        }
-    }
+async function createWorkerFromSource(source: string | Blob, options: WorkerOptions): Promise<WorkerFactory> {
+    const src = typeof source == 'string' ? source : await source.text();
+    const dataUrl = `data:text/javascript,${encodeURIComponent(src)}`;
+    return createWorkerFromUrl(dataUrl, options);
 }
 
 /** Create a worker factory */
-export function createWorkerFactory(source: WorkerSource, options: WorkerOptions = {}): () => MaybeAsync<Worker> {
+export function createWorkerFactory(source: WorkerSource, options: WorkerOptions = {}): WorkerFactory {
     const name = options.name ?? `<anonymous>`;
+
     if (typeof source == 'function') {
+        let factory: WorkerFactory | undefined;
         return async () => {
+            if (factory != null) return await factory();
             const result = await source();
             if (typeof result == 'string' || isBlob(result)) {
-                return await createWorkerFromSource(result, options);
+                factory = await createWorkerFromSource(result, options);
             } else if (isURL(result)) {
-                return await createWorkerFromUrl(result, options);
+                factory = await createWorkerFromUrl(result, options);
             } else if (result != null && typeof result == 'object') {
                 return result as Worker;
             } else {
                 throw new TypeError(`Worker factory of ${name} returned invalid result`);
             }
+            return await factory();
         };
     }
+
+    let factoryBuilder: Promise<WorkerFactory>;
     if (typeof source == 'string' || isBlob(source)) {
-        return async () => createWorkerFromSource(source, options);
+        factoryBuilder = createWorkerFromSource(source, options);
+    } else if (isURL(source)) {
+        factoryBuilder = createWorkerFromUrl(source, options);
+    } else {
+        source satisfies never;
+        throw new TypeError(`Worker source of ${name} is invalid`);
     }
-    if (isURL(source)) {
-        return async () => createWorkerFromUrl(source, options);
-    }
-    source satisfies never;
-    throw new TypeError(`Worker source of ${name} is invalid`);
+    let factory: WorkerFactory | undefined;
+    return async () => {
+        factory ??= await factoryBuilder;
+        return await factory();
+    };
 }
